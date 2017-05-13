f2cbd73b2c8a6dd66e1c61ef147e83878df54bcd
Phuc Le <le.phuc.h@gmail.com>
2017-05-13 16:36:56 -0700

    Fix the default markdown string and add build scripts to project.

--- app/components/App.js
+++ app/components/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      inputValue : "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*"
+      inputValue : "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated by a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, `monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in Spain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*"
     }
 
     this.handleUserInput = this.handleUserInput.bind(this);
